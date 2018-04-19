bfbfe600ed846b66d5fe1a9c11747e4762b12b88
Victor Steinmann <victor.steinmann@jiscom.fr>
2018-04-19 11:38:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://192.168.99.100/loginapp');
+mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
